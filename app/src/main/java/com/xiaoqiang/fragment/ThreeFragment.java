f1300d7314c39c7f440f21821bfc164c7f8865e9
wangqiangqiang <wangqiangqiang1@le.com>
2017-04-09 01:19:54 +0800

    AppCompatActivity/

--- app/src/main/java/com/xiaoqiang/fragment/ThreeFragment.java
+++ app/src/main/java/com/xiaoqiang/fragment/ThreeFragment.java
@@ -1,11 +1,11 @@
 package com.xiaoqiang.fragment;
 
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.xiaoqiang.R;
-import com.xiaoqiang.base.BaseDialog;
 import com.xiaoqiang.base.BaseFragment;
 import com.xiaoqiang.util.ViewUtils;
 
@@ -31,8 +31,14 @@ public class ThreeFragment extends BaseFragment {
         ViewUtils.setAddOnClick(button).subscribe(new Action1() {
             @Override
             public void call(Object o) {
-                BaseDialog dialog = new BaseDialog();
-                dialog.show(getFragmentManager(),"BaseDialog");
+//                BaseDialog dialog = new BaseDialog();
+//                dialog.show(getFragmentManager(),"BaseDialog");
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("好看的对话框");
+                builder.setMessage("这个对话框真好看");
+                builder.setPositiveButton("OK", null);
+                builder.setNegativeButton("Cancel", null);
+                builder.show();
             }
         });
     }
