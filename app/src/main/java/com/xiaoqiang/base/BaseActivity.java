9195027e5d4d634bf47e928551a96b89ceee7e02
wangqiangqiang <wangqiangqiang1@le.com>
2017-04-10 11:20:58 +0800

    Toolbar

--- app/src/main/java/com/xiaoqiang/base/BaseActivity.java
+++ app/src/main/java/com/xiaoqiang/base/BaseActivity.java
@@ -2,25 +2,72 @@ package com.xiaoqiang.base;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.Window;
+import android.widget.Toast;
+
+import com.xiaoqiang.R;
+import com.xiaoqiang.util.ToastUtil;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by xiaoqiang on 2017/3/7.
  */
 
 public abstract class BaseActivity extends AppCompatActivity {
-//    protected View view;
+    private Toolbar toolbar;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        makeActionOverflowMenuShown();
         super.onCreate(savedInstanceState);
         setContentView(loadLayout());
-//        setContentView(R.layout.base_layout);
-//        view = View.inflate(this,loadLayout(), (ViewGroup) findViewById(R.id.main));
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BaseActivity.this, "点击了返回按钮", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.inflateMenu(R.menu.menu_base);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(BaseActivity.this, "点击的菜单", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
         initView();
         initData();
-//        getFragmentManager().beginTransaction().replace();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_base,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    private void makeActionOverflowMenuShown() {
+        //devices with hardware menu button (e.g. Samsung Note) don't show action overflow menu
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception e) {
+
+        }
     }
     protected abstract  int loadLayout();
     protected abstract void initView();
